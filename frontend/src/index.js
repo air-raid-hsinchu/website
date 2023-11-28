55f8ed092f3813f96a04b45e0b5dd124b6d07807
Jay Lee <jayin920805@gmail.com>
2023-11-28 23:35:44 +0800

    feat: layer2 prototype

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,36 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import "./assets/styles/index.css";
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import App from "./App";
+import Layer1 from "./components/Layer1";
+import Layer2 from "./components/Layer2";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <nav className="">
+        <ul className="">
+          <li className="">
+            <Link to="/" className="">Home</Link>
+          </li>
+          <li>
+            <Link to="/layer1" className="">ToDoList</Link>
+          </li>
+          <li>
+            <Link to="/layer2" className="">layer2</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/layer1" element={<Layer1 />} />
+        <Route path="/layer2" element={<Layer2 />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
