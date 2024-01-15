5a3127574544862e130659f34f9dd6502b1285fd
Jay Lee <jayin920805@gmail.com>
2024-01-15 23:30:48 +0800

    layer2 init

--- frontend/src/components/layer1/page3.js
+++ frontend/src/components/layer1/page3.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import img1 from "../../assets/images/1-2.jpg"; // 空襲底下的人民生活
 import img2 from "../../assets/images/1-3.png"; // 二站新竹空襲地圖
 import img3 from "../../assets/images/1-4.jpg"; // 黃旺成與踈開
@@ -88,13 +89,15 @@ const Page3 = () => {
                     backgroundRepeat: 'no-repeat'
                 }}
             >
-                <ImageCard
-                    imgSrc={img1}
-                    imgAlt="空襲底下的人民生活"
-                    imgText={"︿\n空\n襲\n底\n下\n的\n人\n民\n生\n活\n﹀"}
-                    size={{ height: bgImgSize.height * 0.876, width: bgImgSize.width * 0.353 }}
-                    offset={{ y: bgImgOffset.y + bgImgSize.height * 0.062, x: bgImgOffset.x + bgImgSize.width * 0.054 }}
-                />
+                <Link to='/detail2'>
+                    <ImageCard
+                        imgSrc={img1}
+                        imgAlt="空襲底下的人民生活"
+                        imgText={"︿\n空\n襲\n底\n下\n的\n人\n民\n生\n活\n﹀"}
+                        size={{ height: bgImgSize.height * 0.876, width: bgImgSize.width * 0.353 }}
+                        offset={{ y: bgImgOffset.y + bgImgSize.height * 0.062, x: bgImgOffset.x + bgImgSize.width * 0.054 }}
+                    />
+                </Link>
                 <ImageCard
                     imgSrc={img2}
                     imgAlt="二站新竹空襲地圖"
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,13 +7,17 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import Layer1 from "./components/layer1";
+import Page22 from "./components/layer2/page2-2";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Layer1 />} />
+        <Route path='/'>
+          <Route index element={<Layer1 />} /> 
+          <Route path='detail2' element={<Page22 />} />
+        </Route>
       </Routes>
     </HashRouter>
   </React.StrictMode>
