231a67575b5efb656c7b8b60baf0134797c8f1e2
Jay Lee <jayin920805@gmail.com>
2024-01-31 14:15:26 +0800

    fix: mobile page56

--- frontend/src/components/layer1/index.js
+++ frontend/src/components/layer1/index.js
@@ -15,140 +15,178 @@ import Page56 from "./page56";
 import SocialBar from "../socialBar";
 
 const fadeInOptions = {
-    timeout: { enter: 1000, exit: 0 },
+  timeout: { enter: 1000, exit: 0 },
 };
 
 const pageNum = 6;
 
 const Layer1 = () => {
-    const [isMobile, setIsMobile] = useState(false);
-    const elementRefs = useRef(
-        Array(pageNum)
-            .fill()
-            .map(() => createRef())
-    );
-    const [isInViewPort, setIsInViewPort] = useState(Array(pageNum).fill(false));
-    const location = useLocation();
+  const [isMobile, setIsMobile] = useState(false);
+  const elementRefs = useRef(
+    Array(pageNum)
+      .fill()
+      .map(() => createRef())
+  );
+  const [isInViewPort, setIsInViewPort] = useState(Array(pageNum).fill(false));
+  const location = useLocation();
 
-    useEffect(() => {
-        setIsMobile(window.innerHeight > window.innerWidth);
-        handleLoad();
-        window.addEventListener("resize", () => {
-            handleLoad();
-            setIsMobile(window.innerHeight > window.innerWidth);
+  useEffect(() => {
+    setIsMobile(window.innerHeight > window.innerWidth);
+    handleLoad();
+    window.addEventListener("resize", () => {
+      handleLoad();
+      setIsMobile(window.innerHeight > window.innerWidth);
+    });
+  }, [elementRefs.current]);
+
+  const handleLoad = () => {
+    if (location.state && location.state.section) {
+      const section = location.state.section;
+      const sectionElement = document.getElementById(`section${section}`);
+      if (sectionElement) {
+        sectionElement.scrollIntoView({ block: "start" });
+      }
+      setIsInViewPort((prev) => {
+        const newState = [...prev];
+        newState[section - 1] = true;
+
+        return newState;
+      });
+    }
+
+    elementRefs.current.forEach((elementRef) => {
+      const observer = new IntersectionObserver(([entry]) => {
+        setIsInViewPort((prev) => {
+          const newState = [...prev];
+          newState[elementRefs.current.indexOf(elementRef)] =
+            entry.isIntersecting;
+
+          return newState;
         });
-    }, [elementRefs.current]);
+      });
+      if (elementRef.current) {
+        observer.observe(elementRef.current);
+      }
+    });
+  };
 
-    const handleLoad = () => {
-        if (location.state && location.state.section) {
-            const section = location.state.section;
-            const sectionElement = document.getElementById(`section${section}`);
-            if (sectionElement) {
-                sectionElement.scrollIntoView({ block: "start" });
-            }
-            setIsInViewPort((prev) => {
-                const newState = [...prev];
-                newState[section - 1] = true;
+  const scrollToSection2 = () => {
+    const section2 = document.getElementById("section2");
 
-                return newState;
-            });
-        }
+    if (section2) {
+      section2.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
 
-        elementRefs.current.forEach((elementRef) => {
-            const observer = new IntersectionObserver(([entry]) => {
-                setIsInViewPort((prev) => {
-                    const newState = [...prev];
-                    newState[elementRefs.current.indexOf(elementRef)] =
-                        entry.isIntersecting;
-
-                    return newState;
-                });
-            });
-            if (elementRef.current) {
-                observer.observe(elementRef.current);
-            }
-        });
-    };
-
-    const scrollToSection2 = () => {
-        const section2 = document.getElementById("section2");
-
-        if (section2) {
-            section2.scrollIntoView({ behavior: "smooth", block: "start" });
-        }
-    };
-
-    return (
-        <>
+  return (
+    <>
+      {isMobile ? (
+        <div>
+          <Fade
+            in={isInViewPort[3] || isInViewPort[4] || isInViewPort[5]}
+            timeout={fadeInOptions.timeout}
+          >
             <div>
-
-                <Fade in={(isInViewPort[3] || isInViewPort[4] || isInViewPort[5])} timeout={fadeInOptions.timeout}>
-                    <div>
-                        <SocialBar />
-                    </div>
-
-                </Fade>
-                <div className='background'>
-
-                    <Element id="section1" >
-                        <Fade in={isInViewPort[0]} timeout={fadeInOptions.timeout}>
-                            <div ref={elementRefs.current[0]} className='page-container'>
-                                <Page1 handleClick={scrollToSection2} isMobile={isMobile} />
-                            </div>
-                        </Fade>
-                    </Element>
-                    <Element id="section2">
-                        <Fade in={isInViewPort[1]} timeout={fadeInOptions.timeout}>
-                            <div ref={elementRefs.current[1]} className='page-container'>
-                                <Page2 isMobile={isMobile} />
-                            </div>
-                        </Fade>
-
-                    </Element>
-                    <Element id="section3">
-                        <Fade in={isInViewPort[2]} timeout={fadeInOptions.timeout}>
-                            <div ref={elementRefs.current[2]} className='page-container'>
-                                <Page3 isMobile={isMobile} />
-                            </div>
-                        </Fade>
-                    </Element>
-                    <Element id="section4">
-                        <Fade in={isInViewPort[3]} timeout={fadeInOptions.timeout}>
-                            <div ref={elementRefs.current[3]} className='page-container'>
-                                <Page4 isMobile={isMobile} />
-                            </div>
-                        </Fade>
-                    </Element>
-                    {isMobile ? (
-                        <Element id="section5">
-                            <Fade in={isInViewPort[4]} timeout={fadeInOptions.timeout}>
-                                <div ref={elementRefs.current[4]} className="page-container">
-                                    <Page56 />
-                                </div>
-                            </Fade>
-                        </Element>
-                    ) : (
-                        <>
-                            <Element id="section5">
-                                <Fade in={isInViewPort[4]} timeout={fadeInOptions.timeout}>
-                                    <div ref={elementRefs.current[4]} className='page-container'>
-                                        <Page5 />
-                                    </div>
-                                </Fade>
-                            </Element>
-                            <Element id="section6" className='mb-0'>
-                                <Fade in={isInViewPort[5]} timeout={fadeInOptions.timeout}>
-                                    <div ref={elementRefs.current[5]} className='page-container'>
-                                        <Page6 />
-                                    </div>
-                                </Fade>
-                            </Element>
-                        </>
-                    )}
-                </div>
+              <SocialBar />
             </div>
-        </>
-    );
+          </Fade>
+          <div className="background">
+            <Element id="section1">
+              <Fade in={isInViewPort[0]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[0]} className="page-container">
+                  <Page1 handleClick={scrollToSection2} isMobile={isMobile} />
+                </div>
+              </Fade>
+            </Element>
+            <Element id="section2">
+              <Fade in={isInViewPort[1]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[1]} className="page-container">
+                  <Page2 isMobile={isMobile} />
+                </div>
+              </Fade>
+            </Element>
+            <Element id="section3">
+              <Fade in={isInViewPort[2]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[2]} className="page-container">
+                  <Page3 isMobile={isMobile} />
+                </div>
+              </Fade>
+            </Element>
+            <Element id="section4">
+              <Fade in={isInViewPort[3]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[3]} className="page-container">
+                  <Page4 isMobile={isMobile} />
+                </div>
+              </Fade>
+            </Element>
+            <Element id="section5">
+              <Fade in={isInViewPort[4]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[4]} className="page-container">
+                  <Page56 />
+                </div>
+              </Fade>
+            </Element>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <Fade
+            in={isInViewPort[3] || isInViewPort[4] || isInViewPort[5]}
+            timeout={fadeInOptions.timeout}
+          >
+            <div>
+              <SocialBar />
+            </div>
+          </Fade>
+          <div className="background">
+            <Element id="section1">
+              <Fade in={isInViewPort[0]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[0]} className="page-container">
+                  <Page1 handleClick={scrollToSection2} isMobile={isMobile} />
+                </div>
+              </Fade>
+            </Element>
+            <Element id="section2">
+              <Fade in={isInViewPort[1]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[1]} className="page-container">
+                  <Page2 isMobile={isMobile} />
+                </div>
+              </Fade>
+            </Element>
+            <Element id="section3">
+              <Fade in={isInViewPort[2]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[2]} className="page-container">
+                  <Page3 isMobile={isMobile} />
+                </div>
+              </Fade>
+            </Element>
+            <Element id="section4">
+              <Fade in={isInViewPort[3]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[3]} className="page-container">
+                  <Page4 isMobile={isMobile} />
+                </div>
+              </Fade>
+            </Element>
+
+            <Element id="section5">
+              <Fade in={isInViewPort[4]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[4]} className="page-container">
+                  <Page5 />
+                </div>
+              </Fade>
+            </Element>
+            <Element id="section6" className="mb-0">
+              <Fade in={isInViewPort[5]} timeout={fadeInOptions.timeout}>
+                <div ref={elementRefs.current[5]} className="page-container">
+                  <Page6 />
+                </div>
+              </Fade>
+            </Element>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Layer1;
